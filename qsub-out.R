4537f429a4093636a3f4e4b58c6bde9990f78682
Toby Dylan Hocking <Toby.Hocking@nau.edu>
2019-01-14 16:04:05 -0700

    useR overview done

--- qsub-out.R
+++ qsub-out.R
@@ -33,3 +33,66 @@ str(time.df)
 minutes <- with(time.df, hours*60 + minutes + seconds/60)
 quantile(minutes)
 hist(minutes)
+
+## ex 2.
+out.lines <- readLines("qsub-out/BF775_Mono_ATACSeq_2_residuals.out")
+
+epilogue.mat <- str_match_all_variable(
+  tail(out.lines, 15),
+  "\n",
+  name=".*?",
+  ":\\s+",
+  value="\\S+")
+str_match_all_variable(
+  epilogue.mat["Resources", ],
+  name="[^,]+?",
+  "=",
+  value="[^,]+")
+
+## Ex 3.
+out.files <- Sys.glob("qsub-out/*.out")
+out.text.vec <- sapply(out.files, function(f)paste(readLines(f), collapse="\n"))
+
+name.mat <- str_match_variable(
+  out.files,
+  "/",
+  sample_type=list(
+    sample=".*?",
+    "_",
+    type=".*?"
+  ), "_ATAC")
+names(out.text.vec) <- name.mat[, "sample_type"]
+
+int.pattern <- list("[0-9]+", as.integer)
+str_match_variable(
+  out.text.vec,
+  pmem="[0-9]+",
+  "mb,walltime=",
+  hours=int.pattern,
+  ":",
+  minutes=int.pattern,
+  ":",
+  seconds=int.pattern)
+
+hms.pattern <- function(prefix){
+  L <- list(
+    prefix, "=",
+    hours=int.pattern,
+    ":",
+    minutes=int.pattern,
+    ":",
+    seconds=int.pattern)
+  to.rep <- names(L)!=""
+  names(L)[to.rep] <- paste0(prefix, "_", names(L)[to.rep])
+  L
+}
+
+str_match_variable(
+  out.text.vec,
+  hms.pattern("cput"),
+  ",mem=",
+  mem=int.pattern,
+  "kb,vmem=",
+  vmem=int.pattern,
+  "kb,",
+  hms.pattern("walltime"))
